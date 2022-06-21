19cfe876743c65cdbbc8c6a69cad58cf051453f0
RetroSamurai2001 <beatriz.neaime@outlook.com>
2022-06-20 21:06:02 -0300

    versão final

--- js/formulario.js
+++ js/formulario.js
@@ -4,7 +4,8 @@ var div_ano = $(".div-ano")
 var ano = $("#ano")
 const ano_atual = new Date().getFullYear()
 
-ano.on("input", function () {
+ano.on("keyup", function () {
+    $(this).val(this.value.replace(/\D/g, ''))
     if (ano.val().length > 0) {
         if (ano.val() < 1900 || ano.val() > ano_atual) {
             div_ano.removeClass('invisivel')
@@ -22,7 +23,8 @@ var div_valor = $(".div-valor")
 const val_max = 100000000
 var floatt = parseFloat(valor.val())
 
-valor.on("input", function () {
+valor.on("keyup", function () {
+    $(this).val(this.value.replace(/\D/g, ''))
     if (valor.val().length > 0) {
         if (floatt <= 0 || floatt > val_max) {
             div_valor.removeClass('invisivel')
@@ -40,14 +42,24 @@ valor.on("input", function () {
 const ids = ['#marca', '#modelo', '#ano', '#valor', '#cor', '#floatingTextarea']
 const divs = ['.div-marca', '.div-modelo', '.div-ano', '.div-valor', '.div-cor', '.div-desc']
 var botao = $(".enviar")
+var footer = $("footer")
+var cadastrado = $(".enviado")
 
 botao.on("click", function () {
-    for(let i = 0; i<divs.length; i++){
-        if($(ids[i]).val().length < 1){
+    for (let i = 0; i < divs.length; i++) {
+        if ($(ids[i]).val().length < 1) {
             console.log($(divs[i]))
             $(divs[i]).removeClass('invisivel')
-        } else{
+            footer.addClass('invisivel')
+        } else {
             $(divs[i]).addClass('invisivel')
+            footer.removeClass('invisivel')
+            cadastrado.removeClass('invisivel')
+            setTimeout(function () {
+                cadastrado.addClass('invisivel')
+                location.reload()
+            }, 1500)
+
         }
     }
 })
